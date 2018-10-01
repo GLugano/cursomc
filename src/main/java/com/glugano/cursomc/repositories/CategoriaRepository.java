2cd87b7db8f7dcdec0379f4b1a69e888b0dc4855
Lugano <gabrieltomazip@gmail.com>
2018-10-01 16:05:19 -0300

    Categoria

---
+++ src/main/java/com/glugano/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.glugano.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.glugano.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}
